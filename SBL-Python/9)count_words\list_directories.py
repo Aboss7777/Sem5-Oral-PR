d5bcf1e587307401737c54e3f05f5a3b8beccc02
Aboss7777 <96716605+Aboss7777@users.noreply.github.com>
2023-10-31 17:31:18 +0530

    Update 9)count_words\list_directories.py

--- SBL-Python/9)count_words\list_directories.py
+++ SBL-Python/9)count_words\list_directories.py
@@ -1,1 +1,23 @@
+#Program to count words, lines and characters
 
+lines,words,chars=0,0,0
+
+f=open("Example.txt","r")
+for line in f:
+    lines+=1
+    word=line.split()
+    words+=len(word)
+    for char in line:
+        chars+=1
+
+print(f"Number of lines: {lines}")
+print(f"Number of words: {words}")
+print(f"Number of characters: {chars}")
+
+import os
+
+files_list=os.listdir('.')
+
+print("The list of files in current folder are:\n")
+for file in files_list:
+    print(file)
